bfd08648e677a5888f47575ac1fdd85ada5dd30a
Rajesh Aravapalli <rajeshaz09@outlook.com>
2019-04-10 20:11:41 +0530

    tsconfig options

--- src/Greeter.ts
+++ src/Greeter.ts
@@ -5,12 +5,12 @@ interface Person {
 
 class Student {
     fullName: string;
-    constructor(public firstName, public middleInitial, public lastName) {
+    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
         this.fullName = firstName + " " + middleInitial + " " + lastName;
     }
 }
 
-function greeter(person : Person) {
+function greeter(person: Person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
 
