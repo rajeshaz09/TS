6138f8625e3fa12f13213ba05d5d57dd681cf574
Rajesh Aravapalli <rajeshaz09@outlook.com>
2019-04-11 19:26:55 +0530

    async await updated

--- src/Greeter.ts
+++ src/Greeter.ts
@@ -8,8 +8,18 @@ class Student {
     constructor(public firstName: string, public middleInitial: string, public lastName: string) {
         this.fullName = firstName + " " + middleInitial + " " + lastName;
     }
-}
 
+}
+async function myAdd(x: number, y: number) {
+    return x + y;
+}
+async function myAdd2() {
+    let abc = await myAdd(2, 3);
+    return abc;
+}
+myAdd2().then((num: Number) => {
+    console.info(num);
+});
 function greeter(person: Person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
